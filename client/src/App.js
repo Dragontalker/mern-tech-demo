01b2fb0e28b3736e20b7cb11024267b0f52c5d21
Richard <richard.yang.tong@gmail.com>
2021-03-23 10:09:42 -0400

    Added a react form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+
+
+    //JSX
+    return (
+      <div>
+        <h2>Welcome to my App</h2>
+        <form>
+          <div className="form-input">
+            <input
+              type="text"
+              name="title"
+              value=""
+              onChange={} 
+            />
+          </div>
+          <div className="form-input"></div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
