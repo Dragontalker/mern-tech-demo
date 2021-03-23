9188875a622c6766f996a11bad48d08dd44e3e02
Richard <richard.yang.tong@gmail.com>
2021-03-22 20:42:22 -0400

    Modified the MongoDB connection for Heroku deployment.

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mern_template';
 
-mongoose.connect('mongodb://localhost/mern_template', {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
